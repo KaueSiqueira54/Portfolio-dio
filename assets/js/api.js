0e016d3c058efaa1be5814fdce3963503865f333
KaueSiqueira <kaue54150@gmail.com>
2025-08-27 18:04:09 -0300

    Dados do perfil

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,7 @@
-
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json';
-    const response = await fetch(url)
-    const profileData = await response.json()
-    return profileData
+  const url =
+    "https://raw.githubusercontent.com/KaueSiqueira54/Portfolio-dio/refs/heads/main/data/profile.json";
+  const response = await fetch(url);
+  const profileData = await response.json();
+  return profileData;
 }
